613081158c611e094ee627ff2c73814cd5d6138b
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-07-04 08:58:38 -0700

    update semvar, dist, docs

--- dist/src/astro.js
+++ dist/src/astro.js
@@ -22,7 +22,7 @@ export function calibrateGoto(latitude, longitude) {
         lon: longitude,
         lat: latitude,
         date: nowLocal(),
-        path: "DWARF_GOTO_timestamp",
+        path: `DWARF_GOTO_${nowLocalFileName()}`,
     };
     return options;
 }
@@ -42,7 +42,7 @@ export function startGoto(planet, rightAscension, declination, latitude, longitu
         lon: longitude,
         lat: latitude,
         date: nowLocal(),
-        path: "DWARF_GOTO_timestamp",
+        path: `DWARF_GOTO_${nowLocalFileName}`,
     };
     if (planet !== undefined && planet !== null) {
         options.planet = planet;
